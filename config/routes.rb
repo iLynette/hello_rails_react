25d05c8a6f47372cee4dfb9196fce6cf223a43e7
iLynette <lynetteacholah@icloud.com>
2022-07-12 14:25:57 +0300

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api, defaults: {format: 'json'} do
+    get 'greetings', to: 'greetings#index'
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+  get '*page', to: 'static#index'
+  root "static#index"
 end
+
