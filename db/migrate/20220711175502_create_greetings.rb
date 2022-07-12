624ca1db26cd04b1fcd9dbcf3332f190fb255f62
iLynette <lynetteacholah@icloud.com>
2022-07-12 14:19:36 +0300

    Add greetings in table

--- db/migrate/20220711175502_create_greetings.rb
+++ db/migrate/20220711175502_create_greetings.rb
@@ -5,5 +5,10 @@ class CreateGreetings < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    Greeting.create(name: 'Hello one')
+    Greeting.create(name: 'Hello two')
+    Greeting.create(name: 'Hello three')
+    Greeting.create(name: 'Hello four')
+    Greeting.create(name: 'Hello five')
   end
 end
